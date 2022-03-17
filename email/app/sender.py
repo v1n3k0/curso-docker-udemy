6bd1ab13303518dad1158cd57f1a02d4830a7f83
v1n3k0 <v1n1c1u5_1@hotmail.com>
2022-03-17 19:04:48 -0300

    Aula 61

--- email/app/sender.py
+++ email/app/sender.py
@@ -45,10 +45,25 @@ from bottle import Bottle, request, route, run
 #     sender = Sender()
 #     sender.run(host='0.0.0.0', port=8080, debug=True)
 
+DSN = 'dbname=email_sender user=postgres host=db'
+SQL = 'INSERT into emails (assunto, mensagem) values (%s, %s)'
+
+def register_message(assunto, mensagem):
+    conn = psycopg2.connect(DSN)
+    cur = conn.cursor()
+    cur.execute(SQL, (assunto, mensagem))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print('Mesagem registrada !')
+
 @route('/', method='POST')
 def send():
     assunto = request.forms.get('assunto')
     mensagem = request.forms.get('mensagem')
+
+    register_message(assunto, mensagem)
     return 'Mensagem enfileirada! Assunto: {} Mensagem: {}'.format(
         assunto, mensagem
     )
